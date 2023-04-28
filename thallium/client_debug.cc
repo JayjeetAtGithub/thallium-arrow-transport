14c3c4d90b31d4856235b9e9e3bedaab7097207d
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-04-28 10:06:53 -0700

    Add debug code

---
+++ thallium/client_debug.cc
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <fstream>
+
+#include <thallium.hpp>
+
+#include "headers.h"
+#include "payload.h"
+
+
+namespace tl = thallium;
+namespace cp = arrow::compute;
+
+const int32_t kTransferSize = 19 * 1024 * 1024;
+
+
+arrow::Result<ScanReq> GetScanRequest(std::string path,
+                                      cp::Expression filter, 
+                                      std::shared_ptr<arrow::Schema> projection_schema,
+                                      std::shared_ptr<arrow::Schema> dataset_schema) {
+    ARROW_ASSIGN_OR_RAISE(std::shared_ptr<arrow::Buffer> filter_buff, arrow::compute::Serialize(filter));
+    ARROW_ASSIGN_OR_RAISE(auto projection_schema_buff, arrow::ipc::SerializeSchema(*projection_schema));
+    ARROW_ASSIGN_OR_RAISE(auto dataset_schema_buff, arrow::ipc::SerializeSchema(*dataset_schema));
+    ScanReqRPCStub stub(
+        path,
+        const_cast<uint8_t*>(filter_buff->data()), filter_buff->size(), 
+        const_cast<uint8_t*>(dataset_schema_buff->data()), dataset_schema_buff->size(),
+        const_cast<uint8_t*>(projection_schema_buff->data()), projection_schema_buff->size()
+    );
+    ScanReq req;
+    req.stub = stub;
+    req.schema = projection_schema;
+    return req;
+}
+
+ConnCtx Init(std::string protocol, std::string host) {
+    ConnCtx ctx;
+    tl::engine engine(protocol, THALLIUM_SERVER_MODE, true);
+
+    tl::endpoint endpoint = engine.lookup(host);
+    ctx.engine = engine;
+    ctx.endpoint = endpoint;
+    return ctx;
+}
+
+tl::bulk local;
+std::vector<std::pair<void*,std::size_t>> segments(1);
+
+void Scan(ConnCtx &conn_ctx, ScanReq &scan_req) {
+    tl::remote_procedure scan = conn_ctx.engine.define("scan");
+    segments[0].first = (uint8_t*)malloc(kTransferSize);
+    segments[0].second = kTransferSize;
+    local = conn_ctx.engine.expose(segments, tl::bulk_mode::write_only);
+    scan.on(conn_ctx.endpoint)(scan_req.stub);
+}
+
+std::vector<std::shared_ptr<arrow::RecordBatch>> batches;
+
+auto schema = arrow::schema({
+        arrow::field("VendorID", arrow::int64()),
+        arrow::field("tpep_pickup_datetime", arrow::timestamp(arrow::TimeUnit::MICRO)),
+        arrow::field("tpep_dropoff_datetime", arrow::timestamp(arrow::TimeUnit::MICRO)),
+        arrow::field("passenger_count", arrow::int64()),
+        arrow::field("trip_distance", arrow::float64()),
+        arrow::field("RatecodeID", arrow::int64()),
+        arrow::field("store_and_fwd_flag", arrow::utf8()),
+        arrow::field("PULocationID", arrow::int64()),
+        arrow::field("DOLocationID", arrow::int64()),
+        arrow::field("payment_type", arrow::int64()),
+        arrow::field("fare_amount", arrow::float64()),
+        arrow::field("extra", arrow::float64()),
+        arrow::field("mta_tax", arrow::float64()),
+        arrow::field("tip_amount", arrow::float64()),
+        arrow::field("tolls_amount", arrow::float64()),
+        arrow::field("improvement_surcharge", arrow::float64()),
+        arrow::field("total_amount", arrow::float64())
+    });
+
+int64_t total_rows_read = 0;
+
+std::function<void(const tl::request&, std::vector<int32_t>&, std::vector<int32_t>&, std::vector<int32_t>&, std::vector<int32_t>&, std::vector<int32_t>&, int32_t&, tl::bulk&)> f =
+    [&batches, &segments, &local, &schema](const tl::request& req, std::vector<int32_t> &batch_sizes, std::vector<int32_t>& data_offsets, std::vector<int32_t>& data_sizes, std::vector<int32_t>& off_offsets, std::vector<int32_t>& off_sizes, int32_t& total_size, tl::bulk& b) {
+        b(0, total_size).on(req.get_endpoint()) >> local(0, total_size);
+        
+        int num_cols = data_offsets.size() / batch_sizes.size();           
+        for (int32_t batch_idx = 0; batch_idx < batch_sizes.size(); batch_idx++) {
+            int32_t num_rows = batch_sizes[batch_idx];
+            
+            std::vector<std::shared_ptr<arrow::Array>> columns;
+            
+            for (int64_t i = 0; i < num_cols; i++) {
+                int32_t magic_off = (batch_idx * num_cols) + i;
+                std::shared_ptr<arrow::DataType> type = schema->field(i)->type();  
+                if (is_binary_like(type->id())) {
+                    std::shared_ptr<arrow::Buffer> data_buff = arrow::Buffer::Wrap(
+                        (uint8_t*)segments[0].first + data_offsets[magic_off], data_sizes[magic_off]
+                    );
+                    std::shared_ptr<arrow::Buffer> offset_buff = arrow::Buffer::Wrap(
+                        (uint8_t*)segments[0].first + off_offsets[magic_off], off_sizes[magic_off]
+                    );
+
+                    std::shared_ptr<arrow::Array> col_arr = std::make_shared<arrow::StringArray>(num_rows, std::move(offset_buff), std::move(data_buff));
+                    columns.push_back(col_arr);
+                } else {
+                    std::shared_ptr<arrow::Buffer> data_buff = arrow::Buffer::Wrap(
+                        (uint8_t*)segments[0].first  + data_offsets[magic_off], data_sizes[magic_off]
+                    );
+                    std::shared_ptr<arrow::Array> col_arr = std::make_shared<arrow::PrimitiveArray>(type, num_rows, std::move(data_buff));
+                    columns.push_back(col_arr);
+                }
+            }
+            auto batch = arrow::RecordBatch::Make(schema, num_rows, columns);
+            total_rows_read += batch->num_rows();
+            batches.push_back(batch);
+        }
+        return req.respond(0);
+    };
+
+
+arrow::Status Main(int argc, char **argv) {
+    if (argc < 2) {
+        std::cout << "./tc [uri]" << std::endl;
+        exit(1);
+    }
+
+    std::string uri = argv[1];
+
+    auto filter = 
+        cp::greater(cp::field_ref("total_amount"), cp::literal(-200));
+
+    ConnCtx conn_ctx = Init("ofi+verbs", uri);
+    conn_ctx.engine.define("do_rdma", f);
+
+    int64_t total_rows = 0;
+    int64_t total_batches = 0;
+
+    std::string path = "/mnt/cephfs/dataset";
+    ARROW_ASSIGN_OR_RAISE(auto scan_req, GetScanRequest(path, filter, schema, schema));
+
+    Scan(conn_ctx, scan_req);
+    std::cout << "Read " << total_rows_read << " rows" << std::endl;
+
+    conn_ctx.engine.finalize();
+    return arrow::Status::OK();
+}
+
+int main(int argc, char** argv) {
+    Main(argc, argv);
+}
---
+++ thallium/server_debug.cc
@@ -0,0 +1,227 @@
+#include <iostream>
+#include <unordered_map>
+#include <fstream>
+#include <mutex>
+#include <condition_variable>
+
+#include <thallium.hpp>
+
+#include "executor.h"
+
+
+namespace tl = thallium;
+namespace cp = arrow::compute;
+
+
+const int32_t kTransferSize = 19 * 1024 * 1024;
+const int32_t kBatchSize = 1 << 17;
+const std::string kThalliumResultPath = "/proj/schedock-PG0/thallium_result";
+
+void write_to_file(std::string data, std::string path, bool append) {
+    std::ofstream file;
+    if (append) {
+        file.open(path, std::ios_base::app);
+    } else {
+        file.open(path);
+    }
+    file << data;
+    file.close();
+}
+
+
+class ConcurrentRecordBatchQueue {
+    public:
+        std::deque<std::shared_ptr<arrow::RecordBatch>> queue;
+        std::mutex mutex;
+        std::condition_variable cv;
+    
+        void push_back(std::shared_ptr<arrow::RecordBatch> batch) {
+            std::unique_lock<std::mutex> lock(mutex);
+            queue.push_back(batch);
+            lock.unlock();
+            cv.notify_one();
+        }
+
+        void wait_n_pop(std::shared_ptr<arrow::RecordBatch> &batch) {
+            std::unique_lock<std::mutex> lock(mutex);
+            while (queue.empty()) {
+                cv.wait(lock);
+            }
+            batch = queue.front();
+            queue.pop_front();
+        }
+
+        void clear() {
+            std::unique_lock<std::mutex> lock(mutex);
+            queue.clear();
+        }
+};
+
+class RecordBatchQueue {
+    public:
+        std::deque<std::shared_ptr<arrow::RecordBatch>> queue;
+    
+        void push_back(std::shared_ptr<arrow::RecordBatch> batch) {
+            queue.push_back(batch);
+        }
+
+        void wait_n_pop(std::shared_ptr<arrow::RecordBatch> &batch) {
+            while (queue.empty());
+            batch = queue.front();
+            queue.pop_front();
+        }
+};
+
+
+ConcurrentRecordBatchQueue cq;
+void scan_handler(void *arg) {
+    arrow::RecordBatchReader *reader = (arrow::RecordBatchReader*)arg;
+    std::shared_ptr<arrow::RecordBatch> batch;
+    reader->ReadNext(&batch);
+    while (batch != nullptr) {
+        cq.push_back(batch);
+        reader->ReadNext(&batch);
+    }    
+    cq.push_back(nullptr);
+}
+
+
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        std::cout << "./ts [selectivity] [backend]" << std::endl;
+        exit(1);
+    }
+
+    std::string selectivity = argv[1];
+    std::string backend = argv[2];
+
+    tl::engine engine("ofi+verbs", THALLIUM_SERVER_MODE, true);
+    margo_instance_id mid = engine.get_margo_instance();
+    hg_addr_t svr_addr;
+    hg_return_t hret = margo_addr_self(mid, &svr_addr);
+    if (hret != HG_SUCCESS) {
+        std::cerr << "Error: margo_addr_lookup()\n";
+        margo_finalize(mid);
+        return -1;
+    }
+
+    tl::remote_procedure do_rdma = engine.define("do_rdma");    
+    tl::managed<tl::pool> new_pool = tl::pool::create(tl::pool::access::spmc);
+    tl::managed<tl::xstream> xstream = 
+        tl::xstream::create(tl::scheduler::predef::deflt, *new_pool);
+    
+    std::function<void(const tl::request&, const ScanReqRPCStub&)> scan = 
+        [&xstream, &cq, &backend, &engine, &do_rdma, &selectivity](const tl::request &req, const ScanReqRPCStub& stub) {
+            arrow::dataset::internal::Initialize();
+            cp::ExecContext exec_ctx;
+            std::shared_ptr<arrow::RecordBatchReader> reader = ScanDataset(exec_ctx, stub, backend, selectivity).ValueOrDie();
+            auto start = std::chrono::high_resolution_clock::now();
+            
+            bool finished = false;
+            std::vector<std::pair<void*,std::size_t>> segments(1);
+            uint8_t* segment_buffer = (uint8_t*)malloc(kTransferSize);
+            segments[0].first = (void*)segment_buffer;
+            segments[0].second = kTransferSize;
+            tl::bulk arrow_bulk = engine.expose(segments, tl::bulk_mode::read_write);
+            cq.clear();
+
+            xstream->make_thread([&]() {
+                scan_handler((void*)reader.get());
+            }, tl::anonymous());
+
+            std::shared_ptr<arrow::RecordBatch> new_batch;
+
+            while (1 && !finished) {
+                std::vector<std::shared_ptr<arrow::RecordBatch>> batches;
+                std::vector<int32_t> batch_sizes;
+                int64_t rows_processed = 0;
+
+                while (rows_processed < kBatchSize) {
+                    cq.wait_n_pop(new_batch);
+                    if (new_batch == nullptr) {
+                        finished = true;
+                        break;
+                    }
+
+                    rows_processed += new_batch->num_rows();
+
+                    batches.push_back(new_batch);
+                    batch_sizes.push_back(new_batch->num_rows());
+                } 
+
+                if (batches.size() != 0) {
+                    int32_t curr_pos = 0;
+                    int32_t total_size = 0;
+
+                    std::vector<int32_t> data_offsets;
+                    std::vector<int32_t> data_sizes;
+
+                    std::vector<int32_t> off_offsets;
+                    std::vector<int32_t> off_sizes;
+
+                    std::string null_buff = "x";
+                    
+                    for (auto b : batches) {
+                        for (int32_t i = 0; i < b->num_columns(); i++) {
+                            std::shared_ptr<arrow::Array> col_arr = b->column(i);
+                            arrow::Type::type type = col_arr->type_id();
+                            int32_t null_count = col_arr->null_count();
+
+                            if (is_binary_like(type)) {
+                                std::shared_ptr<arrow::Buffer> data_buff = 
+                                    std::static_pointer_cast<arrow::BinaryArray>(col_arr)->value_data();
+                                std::shared_ptr<arrow::Buffer> offset_buff = 
+                                    std::static_pointer_cast<arrow::BinaryArray>(col_arr)->value_offsets();
+
+                                int32_t data_size = data_buff->size();
+                                int32_t offset_size = offset_buff->size();
+
+                                data_offsets.emplace_back(curr_pos);
+                                data_sizes.emplace_back(data_size); 
+                                memcpy(segment_buffer + curr_pos, data_buff->data(), data_size);
+                                curr_pos += data_size;
+
+                                off_offsets.emplace_back(curr_pos);
+                                off_sizes.emplace_back(offset_size);
+                                memcpy(segment_buffer + curr_pos, offset_buff->data(), offset_size);
+                                curr_pos += offset_size;
+
+                                total_size += (data_size + offset_size);
+                            } else {
+                                std::shared_ptr<arrow::Buffer> data_buff = 
+                                    std::static_pointer_cast<arrow::PrimitiveArray>(col_arr)->values();
+
+                                int32_t data_size = data_buff->size();
+                                int32_t offset_size = null_buff.size(); 
+
+                                data_offsets.emplace_back(curr_pos);
+                                data_sizes.emplace_back(data_size);
+                                memcpy(segment_buffer + curr_pos, data_buff->data(), data_size);
+                                curr_pos += data_size;
+
+                                off_offsets.emplace_back(curr_pos);
+                                off_sizes.emplace_back(offset_size);
+                                memcpy(segment_buffer + curr_pos, (uint8_t*)null_buff.c_str(), offset_size);
+                                curr_pos += offset_size;
+
+                                total_size += (data_size + offset_size);
+                            }
+                        }
+                    }
+
+                    segments[0].second = total_size;
+                    do_rdma.on(req.get_endpoint())(batch_sizes, data_offsets, data_sizes, off_offsets, off_sizes, total_size, arrow_bulk);
+                }
+            }
+
+            auto end = std::chrono::high_resolution_clock::now();
+            std::string exec_time_ms = std::to_string((double)std::chrono::duration_cast<std::chrono::microseconds>(end-start).count()/1000) + "\n";
+            write_to_file(exec_time_ms, kThalliumResultPath, true);
+            return req.respond(0);
+        };
+    
+    engine.define("scan", scan);
+    std::string uri_file_path = "/proj/schedock-PG0/thallium_uri";
+    write_to_file(engine.self(), uri_file_path, false);
+    engine.wait_for_finalize();
+};
