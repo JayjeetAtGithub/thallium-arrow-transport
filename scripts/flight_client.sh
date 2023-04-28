94e1aa5997075f18af84c726e663528474b5c161
Jayjeet Chakraborty <jayjeetchakraborty@Jayjeets-MacBook-Pro-2.local>
2023-04-27 20:38:00 -0700

    Make all scripts executable

---
+++ scripts/flight_client.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+binary=$1
+
+function clean_client_cache {
+    sync
+    echo 3 > /proc/sys/vm/drop_caches
+    sync
+}
+
+function clean_server_cache {
+    ssh node1 "sync"
+    ssh node1 "echo 3 > /proc/sys/vm/drop_caches"
+    ssh node1 "sync"
+}
+
+for i in {1..5}; do
+    clean_client_cache
+    clean_server_cache
+    $PWD/bin/$binary || true
+done
