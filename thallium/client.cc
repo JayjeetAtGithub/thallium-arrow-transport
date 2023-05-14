83cbde30e1312a22252f22be74be1d16db59539a
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-05-13 17:47:16 -0700

    Simplify code for duckdb

--- thallium/client.cc
+++ thallium/client.cc
@@ -100,7 +100,7 @@ arrow::Status Main(int argc, char **argv) {
     std::string path = "/mnt/cephfs/dataset";
     std::string query = "SELECT * FROM /mnt/cephfs/dataset";
 
-    Scan(engine, endpoint, scan_req);
+    Scan(engine, endpoint, query);
     std::cout << "Read " << total_rows_read << " rows" << std::endl;
     return arrow::Status::OK();
 }
