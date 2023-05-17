2bc3c802a7275c8b3aab90cef8fb0a3dce612c14
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-05-16 17:57:03 -0700

    fixes

--- flight/client.cc
+++ flight/client.cc
@@ -60,7 +60,7 @@ int main(int argc, char *argv[]) {
   auto end = std::chrono::high_resolution_clock::now();
   
   std::string exec_time_ms = std::to_string((double)std::chrono::duration_cast<std::chrono::microseconds>(end-start).count()/1000) + "\n";
-  WriteToFile(exec_time_ms, kFlightResultPath);
+  WriteToFile(exec_time_ms, kFlightResultPath, true);
 
   std::cout << "Read " << table->num_rows() << " rows and " << table->num_columns() << " columns" << std::endl;
 }
