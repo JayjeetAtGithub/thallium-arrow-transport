67dc9ce911278521a120d3d1831f2b63a56b0b6c
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-05-21 23:04:40 -0700

    Add CLI option to define exec mode

--- scripts/thallium_client.sh
+++ scripts/thallium_client.sh
@@ -4,6 +4,8 @@ set -e
 uri=$(cat /proj/schedock-PG0/thallium_uri)
 echo "Connecting to $uri"
 
+mode=$1
+
 function clean_client_cache {
     sync
     echo 3 > /proc/sys/vm/drop_caches
@@ -21,5 +23,5 @@ query=$(cat /tmp/query)
 for i in {1..5}; do
     clean_client_cache
     clean_server_cache
-    $PWD/bin/tc $uri "/mnt/cephfs/dataset/*" "$query" || true
+    $PWD/bin/tc $uri "/mnt/cephfs/dataset/*" "$query" "$mode" || true
 done
