5b2cfaeef538f381e4f675068c3e88b47cc79710
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-05-30 15:54:50 -0700

    Fix constants

--- src/engine.h
+++ src/engine.h
@@ -1,6 +1,5 @@
 #include <duckdb.hpp>
 #include <duckdb/common/arrow/arrow_converter.hpp>
-#include <duckdb/common/arrow/arrow_wrapper.hpp>
 
 #include <arrow/api.h>
 #include <arrow/c/abi.h>
@@ -9,7 +8,7 @@
 
 class DuckDBRecordBatchReader : public arrow::RecordBatchReader {
     public:
-        DuckDBRecordBatchReader(std::shared_ptr<duckdb::QueryResult> result) : result_(result) {
+        DuckDBRecordBatchReader(std::shared_ptr<duckdb::QueryResult> result) : result(result) {
             auto timezone_config = duckdb::QueryResult::GetConfigTimezone(*result);
             ArrowSchema arrow_schema;
             duckdb::ArrowConverter::ToArrowSchema(&arrow_schema, result->types, result->names, timezone_config);
@@ -17,15 +16,14 @@ class DuckDBRecordBatchReader : public arrow::RecordBatchReader {
         }
 
         arrow::Status ReadNext(std::shared_ptr<arrow::RecordBatch>* out) override {
-            ArrowArray arrow_array;
-            idx_t count = duckdb::ArrowUtil::FetchChunk(result_.get(), 131072, &arrow_array);
-            if (count == 0) {
+            chunk = result->Fetch();
+            if (chunk == nullptr) {
                 *out = nullptr;
                 return arrow::Status::OK();
             }
-
+            ArrowArray arrow_array;
+            duckdb::ArrowConverter::ToArrowArray(*chunk, &arrow_array);
             *out = arrow::ImportRecordBatch(&arrow_array, imported_schema).ValueOrDie();
-            std::cout << "Fetched " << count << " rows" << std::endl;
             return arrow::Status::OK();
         }
 
@@ -34,7 +32,8 @@ class DuckDBRecordBatchReader : public arrow::RecordBatchReader {
         }
 
     protected:
-        std::shared_ptr<duckdb::QueryResult> result_;
+        std::shared_ptr<duckdb::QueryResult> result;
+        std::unique_ptr<duckdb::DataChunk> chunk;
         std::shared_ptr<arrow::Schema> imported_schema;
 };
 
