2c8e9a1c9c3ab9f999721e09c00f6fb11e4b08cf
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-05-29 23:57:53 -0700

    Print batches

--- src/thallium_client.cc
+++ src/thallium_client.cc
@@ -69,6 +69,7 @@ void Scan(ConnCtx &ctx, std::string &path, std::string &query, std::string &mode
                 }
             }
             auto batch = arrow::RecordBatch::Make(schema, num_rows, columns);
+            std::cout << batch->ToString() << std::endl;
         }
         return req.respond(0);
     };
