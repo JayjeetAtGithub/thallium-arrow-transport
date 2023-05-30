0bbb55436cc3cb47d4476a1064cf63eb9d0da412
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-05-30 15:18:05 -0700

    Fix constants

--- src/constants.h
+++ src/constants.h
@@ -2,4 +2,4 @@
 #define BATCH_SIZE 1 << 17
 #define TL_RES_PATH "/proj/schedock-PG0/thallium_results"
 #define TL_URI_PATH "/proj/schedock-PG0/thallium_uri"
-#define FL_RES_PATH "/proj/schedock-PG0/flight_result"
+#define FL_RES_PATH "/proj/schedock-PG0/flight_results"
