6aa8801b7878f7ff9dec60bdfccd68591bb9d13e
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-05-31 13:29:37 -0700

    Check flow

--- src/thallium_server.cc
+++ src/thallium_server.cc
@@ -54,6 +54,7 @@ int main(int argc, char** argv) {
 
     std::function<void(const tl::request&)> start_scan = 
         [&xstream, &engine, &do_rdma, &reader](const tl::request &req) {
+            auto start = std::chrono::high_resolution_clock::now();
             int e;
             bool finished = false;
             std::vector<std::pair<void*,std::size_t>> segments(1);
@@ -157,6 +158,9 @@ int main(int argc, char** argv) {
             }
 
             delete segment_buffer;
+            auto end = std::chrono::high_resolution_clock::now();
+            std::string exec_time_ms = std::to_string((double)std::chrono::duration_cast<std::chrono::microseconds>(end-start).count()/1000) + "\n";
+            std::cout << "Time (ms): " << exec_time_ms << std::endl;
             return req.respond(e);
         };
     
