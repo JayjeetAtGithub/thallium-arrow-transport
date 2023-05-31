8c4c1a699ca1f593e4658ab918cfb093f7cd71bf
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-05-31 11:32:59 -0700

    Measure time on client

--- src/thallium_server.cc
+++ src/thallium_server.cc
@@ -159,7 +159,13 @@ int main(int argc, char** argv) {
             auto end = std::chrono::high_resolution_clock::now();
             std::string exec_time_ms = std::to_string((double)std::chrono::duration_cast<std::chrono::microseconds>(end-start).count()/1000) + "\n";
             std::cout << "Execution time: " << exec_time_ms << std::endl;
+            
+            start = std::chrono::high_resolution_clock::now();
             delete segment_buffer;
+            end = std::chrono::high_resolution_clock::now();
+            exec_time_ms = std::to_string((double)std::chrono::duration_cast<std::chrono::microseconds>(end-start).count()/1000) + "\n";
+            std::cout << "Buffer free time: " << exec_time_ms << std::endl;
+            
             return req.respond(e);
         };
     
