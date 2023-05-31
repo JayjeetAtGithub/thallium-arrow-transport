6100daee7371fee64976dd9243ffb04a20e29507
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-05-30 17:24:24 -0700

    cleanup

--- src/engine.h
+++ src/engine.h
@@ -18,14 +18,13 @@ class DuckDBRecordBatchReader : public arrow::RecordBatchReader {
 
         arrow::Status ReadNext(std::shared_ptr<arrow::RecordBatch>* out) override {
             ArrowArray arrow_array;
-            idx_t count = duckdb::ArrowUtil::FetchChunk(result_.get(), 131072, &arrow_array);
+            idx_t count = duckdb::ArrowUtil::FetchChunk(result_.get(), BATCH_SIZE, &arrow_array);
             if (count == 0) {
                 *out = nullptr;
                 return arrow::Status::OK();
             }
 
             *out = arrow::ImportRecordBatch(&arrow_array, imported_schema).ValueOrDie();
-            std::cout << "Fetched " << count << " rows" << std::endl;
             return arrow::Status::OK();
         }
 
