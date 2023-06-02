a212c8abdf467b48e04fe68aad5e80e3dd148486
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-06-02 11:58:49 -0700

    Simplify code for duckdb

--- src/client.cc
+++ src/client.cc
@@ -15,12 +15,15 @@ arrow::Status Main(int argc, char **argv) {
     std::string query = argv[3];
     std::string mode = argv[4];
 
+    std::cout << uri << std::endl;
+
     tl::engine engine("ofi+verbs", THALLIUM_SERVER_MODE, true);
     tl::endpoint endpoint = engine.lookup(uri);
     tl::remote_procedure scan = engine.define("scan");
 
     std::cout << getTimestamp() << std::endl;
-    scan.on(endpoint)();
+    for (int i = 0; i < 100; i++)
+        scan.on(endpoint)();
     
     return arrow::Status::OK();
 }
