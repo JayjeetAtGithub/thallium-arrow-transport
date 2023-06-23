dd9df048c7fdd9810689c475aaa31688f2ff12a8
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-06-23 00:13:02 -0700

    Fix scripts for tmpfs

--- scripts/deploy_data.sh
+++ scripts/deploy_data.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -ex
 
-sudo mkdir -p /mnt/cephfs/dataset
+sudo mkdir -p /mnt/dataset
+sudo mount -t tmpfs -o size=60G tmpfs /mnt/dataset
 wget https://skyhook-ucsc.s3.us-west-1.amazonaws.com/16MB.uncompressed.parquet
 for i in {1..200}; do
-  sudo cp 16MB.uncompressed.parquet /mnt/cephfs/dataset/16MB.uncompressed.parquet.$i
+  sudo cp 16MB.uncompressed.parquet /mnt/dataset/16MB.uncompressed.parquet.$i
 done
