1ea77e07edeba897c7797317fe6161bf1d01c8d0
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-07-01 19:08:17 -0700

    Add blank RPC

--- src/server.cc
+++ src/server.cc
@@ -4,11 +4,36 @@
 
 namespace tl = thallium;
 
+class GetNextBatchRespStub {
+    public:
+        std::string buffer;
+        int ret_code;
+
+        GetNextBatchRespStub() {}
+        GetNextBatchRespStub(std::string buffer, int ret_code) : buffer(buffer), ret_code(ret_code) {}
+
+        template<typename A>
+        void save(A& ar) const {
+            ar & buffer;
+            ar & ret_code;
+        }
+
+        template<typename A>
+        void load(A& ar) {
+            ar & buffer;
+            ar & ret_code;
+        }
+};
+
 
 std::function<void(const tl::request&, const int&, std::string&)> hello = 
     [](const tl::request &req, const int& a, const std::string &s) {
-        return req.respond(0);
+        GetNextBatchRespStub resp;
+        resp.buffer = "hello world";
+        resp.ret_code = 0;
+        return req.respond(resp);
     };
+
 
 int main(int argc, char** argv) {
 
