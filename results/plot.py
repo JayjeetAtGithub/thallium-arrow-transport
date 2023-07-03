6026de4573a3758b286ed0b939e01302df2c8585
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-07-03 11:01:20 -0700

    Updated plots

--- results/plot.py
+++ results/plot.py
@@ -26,9 +26,9 @@ if __name__ == "__main__":
         data_thallium_duckdb = f.readlines()
         data_thallium_duckdb = [float(x.strip()) for x in data_thallium_duckdb]
 
-    queries_large = [1, 2]
-    queries_medium = [3, 4, 5]
-    queries_small = [6, 7, 8]
+    queries_large = ["1/20.76GB", "2/2.26GB"]
+    queries_medium = ["3/240MB", "4/23.4MB", "5/2.34MB"]
+    queries_small = ["6/220KB", "7/51.2KB", "8/8B"]
 
     # Plot Queries Large
     table = {
@@ -38,18 +38,19 @@ if __name__ == "__main__":
     }
 
     for q in queries_large:
+        a, b = q.split("/")
         for i in range(0, 10):
             table["query"].append(q)
             table["format"].append("flight-acero")
-            table["latency"].append(data_flight_acero[10*(q-1)+i])
+            table["latency"].append(data_flight_acero[10*(int(a)-1)+i])
 
             table["query"].append(q)
             table["format"].append("flight-duckdb")
-            table["latency"].append(data_flight_duckdb[10*(q-1)+i])
+            table["latency"].append(data_flight_duckdb[10*(int(a)-1)+i])
 
             table["query"].append(q)
             table["format"].append("thallium-duckdb")
-            table["latency"].append(data_thallium_duckdb[10*(q-1)+i])
+            table["latency"].append(data_thallium_duckdb[10*(int(a)-1)+i])
 
     df = pd.DataFrame(table)
     print(df)
@@ -63,18 +64,19 @@ if __name__ == "__main__":
     }
 
     for q in queries_medium:
+        a, b = q.split("/")
         for i in range(0, 10):
             table["query"].append(q)
             table["format"].append("flight-acero")
-            table["latency"].append(data_flight_acero[10*(q-1)+i])
+            table["latency"].append(data_flight_acero[10*(int(a)-1)+i])
 
             table["query"].append(q)
             table["format"].append("flight-duckdb")
-            table["latency"].append(data_flight_duckdb[10*(q-1)+i])
+            table["latency"].append(data_flight_duckdb[10*(int(a)-1)+i])
 
             table["query"].append(q)
             table["format"].append("thallium-duckdb")
-            table["latency"].append(data_thallium_duckdb[10*(q-1)+i])
+            table["latency"].append(data_thallium_duckdb[10*(int(a)-1)+i])
 
     df = pd.DataFrame(table)
     print(df)
@@ -88,18 +90,19 @@ if __name__ == "__main__":
     }
 
     for q in queries_small:
+        a, b = q.split("/")
         for i in range(0, 10):
             table["query"].append(q)
             table["format"].append("flight-acero")
-            table["latency"].append(data_flight_acero[10*(q-1)+i])
+            table["latency"].append(data_flight_acero[10*(int(a)-1)+i])
 
             table["query"].append(q)
             table["format"].append("flight-duckdb")
-            table["latency"].append(data_flight_duckdb[10*(q-1)+i])
+            table["latency"].append(data_flight_duckdb[10*(int(a)-1)+i])
 
             table["query"].append(q)
             table["format"].append("thallium-duckdb")
-            table["latency"].append(data_thallium_duckdb[10*(q-1)+i])
+            table["latency"].append(data_thallium_duckdb[10*(int(a)-1)+i])
 
     df = pd.DataFrame(table)
     print(df)
