2bf906597635012c176da97932207ca9c555dd8b
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-07-02 22:08:28 -0700

    Add plots

--- results/plot.py
+++ results/plot.py
@@ -6,24 +6,35 @@ import seaborn as sns
 def plot_graph(df, path):
     sns.set_theme(style="whitegrid")
     sns.set_context("paper", font_scale=1.5)
-    plt = sns.catplot(x="query", y="latency", hue="format", data=df, errwidth=1, capsize=0.1, errorbar="sd", kind="bar", palette="muted")
-    plt.set(xlabel="Query", ylabel="Duration (ms)")
+    plt = sns.catplot(x="query", y="latency", hue="format", data=df, errwidth=1, capsize=0.1, errorbar="sd", kind="bar", palette="muted", legend=False)
+    plt.despine(left=False, bottom=False, top=False, right=False)
+    plt.set(xlabel="Query No.", ylabel="Duration (ms)")
     plt.savefig(path)
 
 
 if __name__ == "__main__":
     # Load data
-    with open('paper/w_o_opt/flight', 'r') as f:
+
+
+    with open('paper/final/flight', 'r') as f:
         data_flight = f.readlines()
         data_flight = [float(x.strip()) for x in data_flight]
 
-    with open('paper/w_o_opt/thallium-optimized', 'r') as f:
-        data_thallium_opt = f.readlines()
-        data_thallium_opt = [float(x.strip()) for x in data_thallium_opt]
+    with open('paper/final/thallium', 'r') as f:
+        data_thallium = f.readlines()
+        data_thallium = [float(x.strip()) for x in data_thallium]
 
-    with open('paper/w_o_opt/thallium-nonoptimized', 'r') as f:
-        data_thallium_noopt = f.readlines()
-        data_thallium_noopt = [float(x.strip()) for x in data_thallium_noopt]
+    # with open('paper/w_o_opt/flight', 'r') as f:
+    #     data_flight = f.readlines()
+    #     data_flight = [float(x.strip()) for x in data_flight]
+
+    # with open('paper/w_o_opt/thallium-optimized', 'r') as f:
+    #     data_thallium_opt = f.readlines()
+    #     data_thallium_opt = [float(x.strip()) for x in data_thallium_opt]
+
+    # with open('paper/w_o_opt/thallium-nonoptimized', 'r') as f:
+    #     data_thallium_noopt = f.readlines()
+    #     data_thallium_noopt = [float(x.strip()) for x in data_thallium_noopt]
 
     queries_large = [1, 2]
     queries_medium = [3, 4, 5]
@@ -43,16 +54,16 @@ if __name__ == "__main__":
             table["latency"].append(data_flight[10*(q-1)+i])
 
             table["query"].append(q)
-            table["format"].append("thallium-no-opt")
-            table["latency"].append(data_thallium_noopt[10*(q-1)+i])
+            table["format"].append("thallium")
+            table["latency"].append(data_thallium[10*(q-1)+i])
 
-            table["query"].append(q)
-            table["format"].append("thallium-opt")
-            table["latency"].append(data_thallium_opt[10*(q-1)+i])
+            # table["query"].append(q)
+            # table["format"].append("thallium-opt")
+            # table["latency"].append(data_thallium_opt[10*(q-1)+i])
 
     df = pd.DataFrame(table)
     print(df)
-    plot_graph(df, "paper/w_o_opt/plot_large.pdf")
+    plot_graph(df, "paper/final/plot_large.pdf")
 
     # Plot Queries Medium
     table = {
@@ -68,16 +79,16 @@ if __name__ == "__main__":
             table["latency"].append(data_flight[10*(q-1)+i])
 
             table["query"].append(q)
-            table["format"].append("thallium-no-opt")
-            table["latency"].append(data_thallium_noopt[10*(q-1)+i])
+            table["format"].append("thallium")
+            table["latency"].append(data_thallium[10*(q-1)+i])
 
-            table["query"].append(q)
-            table["format"].append("thallium-opt")
-            table["latency"].append(data_thallium_opt[10*(q-1)+i])
+            # table["query"].append(q)
+            # table["format"].append("thallium-opt")
+            # table["latency"].append(data_thallium_opt[10*(q-1)+i])
 
     df = pd.DataFrame(table)
     print(df)
-    plot_graph(df, "paper/w_o_opt/plot_medium.pdf")
+    plot_graph(df, "paper/final/plot_medium.pdf")
 
     # Plot Queries Small
     table = {
@@ -93,13 +104,13 @@ if __name__ == "__main__":
             table["latency"].append(data_flight[10*(q-1)+i])
 
             table["query"].append(q)
-            table["format"].append("thallium-no-opt")
-            table["latency"].append(data_thallium_noopt[10*(q-1)+i])
+            table["format"].append("thallium")
+            table["latency"].append(data_thallium[10*(q-1)+i])
 
-            table["query"].append(q)
-            table["format"].append("thallium-opt")
-            table["latency"].append(data_thallium_opt[10*(q-1)+i])
+            # table["query"].append(q)
+            # table["format"].append("thallium-opt")
+            # table["latency"].append(data_thallium_opt[10*(q-1)+i])
 
     df = pd.DataFrame(table)
     print(df)
-    plot_graph(df, "paper/w_o_opt/plot_small.pdf")
+    plot_graph(df, "paper/final/plot_small.pdf")
