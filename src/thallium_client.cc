f32484279f592841fbfc77f4f4f8fccc9e0bde4e
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-07-05 09:23:24 -0700

    Do everything in a single RPC

--- src/thallium_client.cc
+++ src/thallium_client.cc
@@ -69,7 +69,7 @@ class ThalliumClient {
 
             std::shared_ptr<arrow::RecordBatch> batch;
             std::function<void(const tl::request&, int64_t&, std::vector<int64_t>&, std::vector<int64_t>&, tl::bulk&)> do_rdma =
-                [&schema, &batch, &engine](const tl::request& req, int64_t& num_rows, std::vector<int64_t>& data_buff_sizes, std::vector<int64_t>& offset_buff_sizes, tl::bulk& b) {
+                [&schema, &batch, &engine, &total_rows_read](const tl::request& req, int64_t& num_rows, std::vector<int64_t>& data_buff_sizes, std::vector<int64_t>& offset_buff_sizes, tl::bulk& b) {
                     int num_cols = schema->num_fields();
                     
                     std::vector<std::shared_ptr<arrow::Array>> columns;
