ba14c080ea730842a4b47d54a9f5404005935784
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-07-05 09:16:57 -0700

    Do everything in a single RPC

--- src/thallium_server.cc
+++ src/thallium_server.cc
@@ -8,7 +8,7 @@
 
 namespace tl = thallium;
 
-int push_batch(tl::remote_procedure &do_rdma, tl::engine& engine, tl::request& req, std::shared_ptr<arrow::RecordBatch> batch) {
+int push_batch(tl::remote_procedure &do_rdma, tl::engine& engine, const tl::request& req, std::shared_ptr<arrow::RecordBatch> batch) {
     std::vector<int64_t> data_buff_sizes;
     std::vector<int64_t> offset_buff_sizes;
     int64_t num_rows = batch->num_rows();
