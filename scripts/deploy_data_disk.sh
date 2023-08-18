099b122fc5bf68507e7b8e71a114dec6805b42b9
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-08-18 10:46:11 -0700

    Add nyc.parquet

--- scripts/deploy_data_disk.sh
+++ scripts/deploy_data_disk.sh
@@ -2,7 +2,6 @@
 set -ex
 
 sudo mkdir -p /mnt/dataset
-wget https://skyhook-ucsc.s3.us-west-1.amazonaws.com/16MB.uncompressed.parquet
 for i in {1..200}; do
-  sudo cp 16MB.uncompressed.parquet /mnt/dataset/16MB.uncompressed.parquet.$i
+  sudo cp nyc.parquet /mnt/dataset/nyc.parquet.$i
 done
--- scripts/deploy_data_tmpfs.sh
+++ scripts/deploy_data_tmpfs.sh
@@ -3,7 +3,6 @@ set -ex
 
 sudo mkdir -p /mnt/dataset
 sudo mount -t tmpfs -o size=60G tmpfs /mnt/dataset
-wget https://skyhook-ucsc.s3.us-west-1.amazonaws.com/16MB.uncompressed.parquet
 for i in {1..200}; do
-  sudo cp 16MB.uncompressed.parquet /mnt/dataset/16MB.uncompressed.parquet.$i
+  sudo cp nyc.parquet /mnt/dataset/nyc.parquet.$i
 done
