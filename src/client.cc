52c61ae74ec3d393acdde183198f6071eb1ca25e
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-08-30 18:56:35 -0700

    Fix arg

--- src/client.cc
+++ src/client.cc
@@ -5,6 +5,27 @@
 
 namespace tl = thallium;
 
+class RespStub {
+    public:
+        std::string buffer;
+        int ret_code;
+
+        RespStub() {}
+        RespStub(std::string buffer, int ret_code) : buffer(buffer), ret_code(ret_code) {}
+
+        template<typename A>
+        void save(A& ar) const {
+            ar & buffer;
+            ar & ret_code;
+        }
+
+        template<typename A>
+        void load(A& ar) {
+            ar & buffer;
+            ar & ret_code;
+        }
+};
+
 int main(int argc, char** argv) {
 
     if(argc < 2) {
@@ -23,7 +44,8 @@ int main(int argc, char** argv) {
     std::cout << "Warmup done" << std::endl;
     for (int i = 0; i < 1000; i++) {
         auto start = std::chrono::high_resolution_clock::now();
-        hello.on(server)(2, s);
+        RespStub resp = hello.on(server)(2, s);
+        std::cout << "Size of response: " << resp.buffer.size() << std::endl;
         auto end = std::chrono::high_resolution_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end-start).count();
         std::cout << "Iteration " << i << " took " << duration << " microseconds" << std::endl;
