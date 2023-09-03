0dcd07f70798211411bdf0fb4b5850f1a1c0f322
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-09-03 00:39:00 -0700

    Add script to generate pq file

--- scripts/write_dataset.py
+++ scripts/write_dataset.py
@@ -2,7 +2,8 @@ import pyarrow.parquet as pq
 import pandas as pd
 
 if __name__ == "__main__":
-    df = pq.read_table("data.parquet").to_pandas()
+    df = pq.read_table("nyc.parquet").to_pandas()
     for i in range(5):
+        print("Appending ", i)
         df = df.append(df)
     df.to_parquet("nyc.parquet", compression=None)
