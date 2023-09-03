943891def4d1b0000bcf27bf18f56f3f09c897f0
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-09-03 00:25:42 -0700

    Add updated scripts

--- scripts/deploy_data_disk.sh
+++ scripts/deploy_data_disk.sh
@@ -2,6 +2,4 @@
 set -ex
 
 sudo mkdir -p /mnt/dataset
-for i in {1..32}; do
-  sudo cp nyc.parquet /mnt/dataset/nyc.parquet.$i
-done
+sudo cp nyc.parquet /mnt/dataset/nyc.parquet
--- scripts/deploy_data_tmpfs.sh
+++ scripts/deploy_data_tmpfs.sh
@@ -3,6 +3,4 @@ set -ex
 
 sudo mkdir -p /mnt/dataset
 sudo mount -t tmpfs -o size=60G tmpfs /mnt/dataset
-for i in {1..32}; do
-  sudo cp nyc.parquet /mnt/dataset/nyc.parquet.$i
-done
+sudo cp nyc.parquet /mnt/dataset/nyc.parquet
