793e60e30e1758770183280cf6c5ad0949b8415a
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-11-10 10:35:22 -0800

    add shortcuts

--- src/utils.h
+++ src/utils.h
@@ -127,15 +127,15 @@ class IterateRespStub {
         IterateRespStub() {}
         IterateRespStub(std::shared_ptr<arrow::RecordBatch> batch, int ret_code) : batch(batch), ret_code(ret_code) {}
 
-        template<typename A>
-        void save(A& ar, std::shared_ptr<arrow::RecordBatch> &batch) const {
+        template<typename Archive>
+        void save(Archive& ar, std::shared_ptr<arrow::RecordBatch> &batch) const {
             ThalliumOutputStreamAdaptor<Archive> output_stream{ar};
 	        arrow::ipc::SerializeRecordBatch(batch, options, &output_stream);
         }
 
-        template<typename A>
-        void load(A& ar, std::shared_ptr<arrow::RecordBatch> &batch) {
-            ThalliumInputStreamAdaptor<Archive> input_stream{ar};
+        template<typename Archive>
+        void load(Archive& ar, std::shared_ptr<arrow::RecordBatch> &batch) {
+            ThalliumInputStreamAdaptor<A> input_stream{ar};
             arrow::ipc::DictionaryMemo dict_memo;
             arrow::ipc::IpcReadOptions options;
             auto schema = arrow::ipc::ReadSchema(&input_stream, &dict_memo).ValueOrDie();
