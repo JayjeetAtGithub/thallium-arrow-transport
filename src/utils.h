4ae3656974a644e0ce6d37c0a67a79fbbec4eb89
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-11-10 10:17:46 -0800

    add shortcuts

--- src/utils.h
+++ src/utils.h
@@ -80,6 +80,76 @@ class IterateRespStub {
         }
 };
 
+template<typename Archive>
+struct ThalliumOutputStreamAdaptor : public arrow::io::OutputStream {
+	ThalliumOutputStreamAdaptor(Archive& ar)
+	: m_archive(ar) {}
+
+	arrow::Status Close() override {
+	    m_closed = true;
+		return arrow::Status::OK();
+	}
+	
+	Result<int64_t> Tell() const override {
+		return m_written;
+	}
+	
+	bool closed() const override {
+		return m_closed;
+	}
+	
+	arrow::Status Write(const void* data, int64_t nbytes) override {
+		m_written += nbytes;
+		m_archive.write(static_cast<const char*>(data), static_cast<size_t>(nbytes));
+		return arrow::Status::OK();
+	}
+	
+	Archive& m_archive;
+	int64_t  m_written = 0;
+	bool     m_closed = false;
+};
+
+template<typename Archive>
+struct ThalliumInputStreamAdaptor : public arrow::io::InputStream {
+
+	ThalliumInputStreamAdaptor(Archive& ar)
+	: m_archive(ar) {}
+
+	arrow::Status Close() override {
+		m_closed = true;
+		return arrow::Status::OK();
+	}
+	
+	Result<int64_t> Tell() const override {
+		return m_read;
+	}
+	
+	bool closed() const override {
+		return m_closed;
+	}
+	
+	Result<int64_t> Read(int64_t nbytes, void* out) override {
+		m_archive.read(static_cast<const char*>(data), static_cast<size_t>(nbytes));
+        m_read += nbytes;
+		return nbytes; // not sure this is the way to return a Result<T>
+	}
+	
+	Result<std::shared_ptr<Buffer>> Read(int64_t nbytes) override {
+		// I *think* we should be able to just do that, since zero-copy is
+		// not supported by out InputStream, it should not be called...
+		return nullptr;
+	}
+	
+	bool supports_zero_copy() const override {
+		return false;
+	}
+	
+	Archive& m_archive;
+	int64_t  m_read = 0;
+	bool     m_closed = false;
+
+};
+
 std::shared_ptr<arrow::Buffer> PackBatch(std::shared_ptr<arrow::RecordBatch> batch) {
     arrow::ipc::IpcWriteOptions options;
     return arrow::ipc::SerializeRecordBatch(*batch, options).ValueOrDie();
