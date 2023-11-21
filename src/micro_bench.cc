6b2510241162919801dfdf34cb3be467a411401c
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-11-21 10:23:09 -0800

    Add microbenchs

--- src/micro_bench.cc
+++ src/micro_bench.cc
@@ -19,6 +19,6 @@ int main(int argc, char **argv) {
     while (reader->ReadNext(&batch).ok()) {
         std::cout << "batch size: " << batch->num_rows() << std::endl;
         arrow::ipc::IpcWriteOptions options;
-        arrow::ipc::WriteRecordBatchStream(std::vector<std::shared_ptr<arrow::RecordBatch>>{batch}, options, output_stream.get());
+        // arrow::ipc::WriteRecordBatchStream(std::vector<std::shared_ptr<arrow::RecordBatch>>{batch}, options, output_stream.get());
     }
 }
