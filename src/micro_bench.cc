874dbf031a3882c3e0dd4850c29679c61ac82152
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-11-21 10:56:51 -0800

    Add microbenchs

--- src/micro_bench.cc
+++ src/micro_bench.cc
@@ -16,13 +16,14 @@ int main(int argc, char **argv) {
     // read out batches from the tables and serializd each of them
     std::shared_ptr<arrow::RecordBatch> batch;
     auto output_stream = arrow::io::BufferOutputStream::Create().ValueOrDie();
+    auto writer = arrow::ipc::MakeStreamWriter(output_stream, reader->schema()).ValueOrDie();
     while (reader->ReadNext(&batch).ok()) {
         if (batch) {
             std::cout << "batch size: " << batch->num_rows() << std::endl;
-            arrow::ipc::IpcWriteOptions options;
-            arrow::ipc::WriteRecordBatchStream(std::vector<std::shared_ptr<arrow::RecordBatch>>{batch}, options, output_stream.get());
+            writer->WriteRecordBatch(batch);
         }
     }
+    writer->Close();
     arrow::Result<std::shared_ptr<arrow::Buffer>> buffer = output_stream->Finish();
     if (!buffer.ok()) {
         std::cout << "Error: " << buffer.status().message() << std::endl;
