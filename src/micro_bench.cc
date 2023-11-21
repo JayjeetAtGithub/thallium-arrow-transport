7b7b754fd987cafab8bb93e22b8298b2e2c572e3
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-11-21 11:06:33 -0800

    Add microbenchs

--- src/micro_bench.cc
+++ src/micro_bench.cc
@@ -6,16 +6,8 @@
 #include "engine.h"
 #include "constants.h"
 
-int main(int argc, char **argv) {
-    // read the parquet file into arrow tables
-    std::shared_ptr<DuckDBEngine> db = std::make_shared<DuckDBEngine>();
-    db->Create("/mnt/dataset/nyc.parquet");
-    std::string query = "SELECT * FROM dataset WHERE total_amount >= 900.5;"; 
-    std::shared_ptr<arrow::RecordBatchReader> reader = db->Execute(query);
-
-    // read out batches from the tables and serializd each of them
+void WriteToStream(std::shared_ptr<arrow::OutputStream> &output_stream, std::shared_ptr<arrow::RecordBatchReader> &reader) {
     std::shared_ptr<arrow::RecordBatch> batch;
-    auto output_stream = arrow::io::BufferOutputStream::Create().ValueOrDie();
     auto writer = arrow::ipc::MakeStreamWriter(output_stream, reader->schema()).ValueOrDie();
     while (reader->ReadNext(&batch).ok()) {
         if (!batch) {
@@ -31,5 +23,18 @@ int main(int argc, char **argv) {
         return 1;
     }
     std::cout << "Size: " << buffer.ValueOrDie()->size() << std::endl;
+}
+
+int main(int argc, char **argv) {
+    // read the parquet file into arrow tables
+    std::shared_ptr<DuckDBEngine> db = std::make_shared<DuckDBEngine>();
+    db->Create("/mnt/dataset/nyc.parquet");
+    std::string query = "SELECT * FROM dataset WHERE total_amount >= 900.5;"; 
+    std::shared_ptr<arrow::RecordBatchReader> reader = db->Execute(query);
+
+    // read out batches from the tables and serializd each of them
+    auto output_stream = arrow::io::BufferOutputStream::Create().ValueOrDie();
+    WriteToStream(output_stream, reader);
+
     return 0;
 }
