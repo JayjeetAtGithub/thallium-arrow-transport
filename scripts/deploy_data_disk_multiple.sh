e5ba74589b3d6754b21c734824fa91df1ac92b21
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-12-01 21:32:59 -0800

    fix the naming of parquet files

--- scripts/deploy_data_disk_multiple.sh
+++ scripts/deploy_data_disk_multiple.sh
@@ -5,5 +5,5 @@ sudo mkdir -p /mnt/dataset
 
 # write the files couple number of times
 for i in {1..105}; do
-    sudo cp nyc.parquet /mnt/dataset/nyc.parquet.$i
+    sudo cp nyc.parquet /mnt/dataset/nyc.$i.parquet
 done
