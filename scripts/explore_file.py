c1e28d2887779bd39a3afb7423953c82e40e1172
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-12-01 20:23:40 -0800

    Add script to explore table content

--- scripts/explore_file.py
+++ scripts/explore_file.py
@@ -5,5 +5,6 @@ import pyarrow.parquet as pq
 if __name__ == "__main__":
     filename = str(sys.argv[1])
     table = pq.read_table(filename)
-    print("Table Size: ", table.nbytes)
+    print("Table size in Bytes: ", table.nbytes)
+    print("Table size in MB: ", table.nbytes / 1024 / 1024)
 
