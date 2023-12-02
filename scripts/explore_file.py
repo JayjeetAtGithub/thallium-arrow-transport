2528733ffe58a76096f7741aae44e8696348dd0a
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-12-01 20:23:59 -0800

    Add script to explore table content

--- scripts/explore_file.py
+++ scripts/explore_file.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     filename = str(sys.argv[1])
     table = pq.read_table(filename)
     print("Table size in Bytes: ", table.nbytes)
-    print("Table size in MB: ", table.nbytes / 1024 / 1024)
+    print("Table size in MB: ", table.nbytes / 1024 / 1024, " MB")
 
