4621214e84603aba93012a607e697c1ccdffbab4
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-12-02 20:35:19 -0800

    Write 50 GB of dataset

--- scripts/deploy_data_disk_multiple.sh
+++ scripts/deploy_data_disk_multiple.sh
@@ -4,6 +4,8 @@ set -ex
 sudo mkdir -p /mnt/dataset
 
 # write the files couple number of times
+# 5.88 GB total dataset size
+# 50.4 GB total table size Apache Arrow format
 for i in {1..105}; do
     sudo cp nyc.parquet /mnt/dataset/nyc.$i.parquet
 done
