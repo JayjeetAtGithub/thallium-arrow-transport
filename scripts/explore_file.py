2a0e102f7fdac5a17f162e664cdb43d483e6986e
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-12-02 22:02:18 -0800

    Add more column name and types

--- scripts/explore_file.py
+++ scripts/explore_file.py
@@ -7,4 +7,8 @@ if __name__ == "__main__":
     table = pq.read_table(filename)
     print("Table size in Bytes: ", table.nbytes)
     print("Table size in MB: ", table.nbytes / 1024 / 1024, " MB")
-
+    print("Number of rows: ", table.num_rows)
+    print("Number of columns: ", table.num_columns)
+    print("Schema: ", table.schema)
+    print("Column names: ", table.column_names)
+    print("Column types: ", table.column_types)
