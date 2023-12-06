538b64f5ca41b7928ef880c27762fd99d950fa63
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-12-05 19:27:40 -0800

    Add new explore file script

--- scripts/explore_file.py
+++ scripts/explore_file.py
@@ -4,11 +4,13 @@ import pyarrow.parquet as pq
 
 def describe_table(table):
     print("Table size in Bytes: ", table.nbytes)
-    print("Table size in MB: ", table.nbytes / 1024 / 1024, " MB")
+    table_size_in_mb = table.nbytes / 1024 / 1024
+    print("Table size in MB: ", table_size_in_mb, " MB")
     print("Number of rows: ", table.num_rows)
     print("Number of columns: ", table.num_columns)
     print("Schema: ", table.schema)
     print("Column names: ", table.column_names)
+    print("Total dataset size for 105 files: ", table_size_in_mb * 105, " MB")
 
 
 if __name__ == "__main__":
