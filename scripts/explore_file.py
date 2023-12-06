a8beb8f388034dc9284cf21ed225c596c561339d
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-12-05 19:25:27 -0800

    Add new explore file script

--- scripts/explore_file.py
+++ scripts/explore_file.py
@@ -2,9 +2,7 @@ import sys
 import pyarrow.parquet as pq
 
 
-if __name__ == "__main__":
-    filename = str(sys.argv[1])
-    table = pq.read_table(filename)
+def describe_table(table):
     print("Table size in Bytes: ", table.nbytes)
     print("Table size in MB: ", table.nbytes / 1024 / 1024, " MB")
     print("Number of rows: ", table.num_rows)
@@ -12,6 +10,27 @@ if __name__ == "__main__":
     print("Schema: ", table.schema)
     print("Column names: ", table.column_names)
 
+
+if __name__ == "__main__":
+    filename = str(sys.argv[1])
+
+    filters_expr = [
+        ('total_amount', '>=', 63),
+        ('total_amount', '>=', 104),
+        ('total_amount', '>=', 178),
+        ('total_amount', '>=', 335),
+        ('total_amount', '>=', 530),
+        ('total_amount', '>=', 1030),
+    ]
+
+
+    table = pq.read_table(filename)
+    describe_table(table)
+
+    for expr in filters_expr:
+        print("Filter: ", expr)
+        filtered_table = pq.read_table(filename, filters=[expr])
+        describe_table(filtered_table)
 
 # 10% total_amount >= 63, 350000
 # 1%  total_amount >= 104, 35000
