79aefa1301dc1a2ffd89b92ebc44472d6a522b73
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-12-07 13:57:53 -0800

    Comment on the client code

--- src/client.cc
+++ src/client.cc
@@ -2,9 +2,7 @@
 #include <chrono>
 #include <thallium/serialization/stl/string.hpp>
 
-
 namespace tl = thallium;
-
 
 int main(int argc, char** argv) {
     if(argc < 2) {
@@ -12,33 +10,50 @@ int main(int argc, char** argv) {
         exit(0);
     }
 
+    // Read the server uri from the user
     std::string uri = argv[1];
 
+    // Define thallium client engine and lookup server endpoint
     tl::engine engine("ofi+verbs", THALLIUM_SERVER_MODE);
     tl::endpoint endpoint = engine.lookup(uri);
+
+    // Declare the `get_single_byte` remote procedure
     tl::remote_procedure get_single_byte = engine.define("get_single_byte");
 
+    // Define the `do_rdma` remote procedure
     std::function<void(const tl::request&, const tl::bulk&)> do_rdma = 
         [&engine](const tl::request &req, const tl::bulk &bulk) {
         std::cout << "do_rdma" << std::endl;
 
+        // Reserve a single segment
         std::vector<std::pair<void*,std::size_t>> segments;
         segments.reserve(1);
 
+        // Allocate memory for a single char and add it to the segment
         char *single_char = new char[1];
         segments.emplace_back(std::make_pair((void*)(&single_char[0]), 1));
         
+        // Expose the segment as a local bulk handle
         tl::bulk local = engine.expose(segments, tl::bulk_mode::write_only);
+
+        // Pull the single byte from the remote bulk handle
         bulk.on(req.get_endpoint()) >> local;
+        
+        // Respond back with 0
         return req.respond(0);
     };
+    // Define the `do_rdma` procedure
     engine.define("do_rdma", do_rdma);
 
+    // Do 50 iterations to warmup the server.
+    // TODO: Look into thallium for why the first couple requests
+    // are slow, mostly due to scheduling issues.
     for (int i = 0; i < 50; i++) {
         get_single_byte.on(endpoint)(1);
     }
-
     std::cout << "Warmup done" << std::endl;
+    
+    // Run 1000 iterations of reading a single byte from the server
     for (int i = 0; i < 1000; i++) {
         auto start = std::chrono::high_resolution_clock::now();
         get_single_byte.on(endpoint)(0);
