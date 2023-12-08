3a6ca0027a5f8a398684b1d701591233b32dddc6
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-12-07 17:07:43 -0800

    add rdma fast path

--- src/server.cc
+++ src/server.cc
@@ -16,9 +16,22 @@ int main(int argc, char** argv) {
     // Declare the `do_rdma` remote procedure
     tl::remote_procedure do_rdma = engine.define("do_rdma");
 
+
+    // DuckDB query
+    std::string query = "SELECT * FROM dataset WHERE total_amount >= 1030;";
+    std::string path = "/mnt/dataset/nyc.1.parquet";
+    std::shared_ptr<DuckDBEngine> db = std::make_shared<DuckDBEngine>();
+    db->Create(path);
+    std::shared_ptr<arrow::RecordBatchReader> reader = db->Execute(query);
+    std::shared_ptr<arrow::RecordBatch> batch;
+    reader->ReadNext(&batch);
+    auto buff = PackBatch(batch);
+    std::cout << "Batch size: " << buff->size() << std::endl;
+
+
     // Define the `get_data_bytes` procedure
     std::function<void(const tl::request&, const int&)> get_data_bytes = 
-    [&do_rdma, &engine, &data_size](const tl::request &req, const int& warmup) {
+    [&do_rdma, &engine, &data_size, &buff](const tl::request &req, const int& warmup) {
         // If warmup, then just return
         if (warmup == 1) {
             std::cout << "Warmup" << std::endl;
@@ -31,14 +44,7 @@ int main(int argc, char** argv) {
         std::vector<std::pair<void*,std::size_t>> segments;
         segments.reserve(1);
         
-        // Map the buffer for the single char to the segment
-        std::string data = "";
-        // Append `n` chars to the string
-        for (int32_t i = 0; i < data_size; i++) {
-            data += "x";
-        }
-        segments.emplace_back(std::make_pair((void*)(&data[0]), data.size()));
-
+        segments.emplace_back(std::make_pair((void*)buff->data(), buff->size()));
         // Expose the segment and send it as argument to `do_rdma`
         tl::bulk bulk = engine.expose(segments, tl::bulk_mode::read_only);
         do_rdma.on(req.get_endpoint())(bulk);
