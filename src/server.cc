52867b769023e945fbf0856c41352369a021e861
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-12-10 15:19:00 -0800

    Add client and server code

--- src/server.cc
+++ src/server.cc
@@ -49,54 +49,57 @@ int main(int argc, char** argv) {
         auto reader = reader_map[0];
         std::shared_ptr<arrow::RecordBatch> batch;
         reader->ReadNext(&batch);
+        if (batch != nullptr) {
+            std::vector<int64_t> data_buff_sizes;
+            std::vector<int64_t> offset_buff_sizes;
+            int64_t num_rows = batch->num_rows();
 
-        std::vector<int64_t> data_buff_sizes;
-        std::vector<int64_t> offset_buff_sizes;
-        int64_t num_rows = batch->num_rows();
+            std::vector<std::pair<void*,std::size_t>> segments;
+            segments.reserve(batch->num_columns()*2);
 
-        std::vector<std::pair<void*,std::size_t>> segments;
-        segments.reserve(batch->num_columns()*2);
+            std::string null_buff = "x";
 
-        std::string null_buff = "x";
+            for (int64_t i = 0; i < batch->num_columns(); i++) {
+                std::shared_ptr<arrow::Array> col_arr = batch->column(i);
 
-        for (int64_t i = 0; i < batch->num_columns(); i++) {
-            std::shared_ptr<arrow::Array> col_arr = batch->column(i);
+                int64_t data_size = 0;
+                int64_t offset_size = 0;
 
-            int64_t data_size = 0;
-            int64_t offset_size = 0;
+                if (is_base_binary_like(col_arr->type_id())) {
+                    std::shared_ptr<arrow::Buffer> data_buff = 
+                        std::static_pointer_cast<arrow::BinaryArray>(col_arr)->value_data();
+                    std::shared_ptr<arrow::Buffer> offset_buff = 
+                        std::static_pointer_cast<arrow::BinaryArray>(col_arr)->value_offsets();
+                    
+                    data_size = data_buff->size();
+                    offset_size = offset_buff->size();
+                    segments.emplace_back(std::make_pair((void*)data_buff->data(), data_size));
+                    segments.emplace_back(std::make_pair((void*)offset_buff->data(), offset_size));
+                } else {
 
-            if (is_base_binary_like(col_arr->type_id())) {
-                std::shared_ptr<arrow::Buffer> data_buff = 
-                    std::static_pointer_cast<arrow::BinaryArray>(col_arr)->value_data();
-                std::shared_ptr<arrow::Buffer> offset_buff = 
-                    std::static_pointer_cast<arrow::BinaryArray>(col_arr)->value_offsets();
-                
-                data_size = data_buff->size();
-                offset_size = offset_buff->size();
-                segments.emplace_back(std::make_pair((void*)data_buff->data(), data_size));
-                segments.emplace_back(std::make_pair((void*)offset_buff->data(), offset_size));
-            } else {
+                    std::shared_ptr<arrow::Buffer> data_buff = 
+                        std::static_pointer_cast<arrow::PrimitiveArray>(col_arr)->values();
 
-                std::shared_ptr<arrow::Buffer> data_buff = 
-                    std::static_pointer_cast<arrow::PrimitiveArray>(col_arr)->values();
+                    data_size = data_buff->size();
+                    offset_size = null_buff.size(); 
+                    segments.emplace_back(std::make_pair((void*)data_buff->data(), data_size));
+                    segments.emplace_back(std::make_pair((void*)(&null_buff[0]), offset_size));
+                }
 
-                data_size = data_buff->size();
-                offset_size = null_buff.size(); 
-                segments.emplace_back(std::make_pair((void*)data_buff->data(), data_size));
-                segments.emplace_back(std::make_pair((void*)(&null_buff[0]), offset_size));
+                data_buff_sizes.push_back(data_size);
+                offset_buff_sizes.push_back(offset_size);
             }
 
-            data_buff_sizes.push_back(data_size);
-            offset_buff_sizes.push_back(offset_size);
+            // Expose the segment and send it as argument to `do_rdma`
+            auto s = std::chrono::high_resolution_clock::now();
+            tl::bulk bulk = engine.expose(segments, tl::bulk_mode::read_only);
+            auto e = std::chrono::high_resolution_clock::now();
+            auto d = std::chrono::duration_cast<std::chrono::microseconds>(e-s).count();
+            std::cout << "expose took " << d << " microseconds" << std::endl;
+            do_rdma.on(req.get_endpoint())(num_rows, data_buff_sizes, offset_buff_sizes, bulk);
+        } else {
+            std::cout << "No more batches" << std::endl;
         }
-
-        // Expose the segment and send it as argument to `do_rdma`
-        auto s = std::chrono::high_resolution_clock::now();
-        tl::bulk bulk = engine.expose(segments, tl::bulk_mode::read_only);
-        auto e = std::chrono::high_resolution_clock::now();
-        auto d = std::chrono::duration_cast<std::chrono::microseconds>(e-s).count();
-        std::cout << "expose took " << d << " microseconds" << std::endl;
-        do_rdma.on(req.get_endpoint())(num_rows, data_buff_sizes, offset_buff_sizes, bulk);
 
         // Respond back with 0
         return req.respond(0);
