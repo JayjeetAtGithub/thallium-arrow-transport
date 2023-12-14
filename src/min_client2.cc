ec9192109f4c4f8278e5fc1bd981fe7ce6ff4277
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-12-13 22:08:07 -0800

    Add multiple strings

--- src/min_client2.cc
+++ src/min_client2.cc
@@ -72,7 +72,7 @@ int main(int argc, char** argv) {
     engine.define("do_rdma", do_rdma);
 
     // Run 50 iterations of reading a single byte from the server
-    for (int i = 0; i < 200; i++) {
+    for (int i = 0; i < 400; i++) {
         call_init_scan_rpc(init_scan, endpoint);
         call_get_data_bytes_rpc(get_data_bytes, endpoint);
     }
